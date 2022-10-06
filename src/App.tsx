5754315de04208a8ae6eb1f4645c510d39270c3d
gunwoongPark <bung1438@gmail.com>
2022-10-06 16:43:50 +0900

    feat(app): list 출력 및 css 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ul className="list-container">
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+        <li className="element">1</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
